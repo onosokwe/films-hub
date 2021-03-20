35782314c9f66ae94b9d96ba5a33f8f191db5d65
Andrew <57761772+onosokwe@users.noreply.github.com>
2021-03-20 10:13:42 +0100

    Update nav.php

--- admin/inc/nav.php
+++ admin/inc/nav.php
@@ -1,6 +1,7 @@
 <nav>
-	<a href="./"><img src="../files/img/logo.png" width="110" height="35"></a>
-    <a href="./">Films</a>
+	<a href="./"><img src="../files/img/logo.png" width="110" height="35" /></a>
+	<a href="./">Films</a>
+	<a href="./users">Users</a>
     <a href="./genres">Genres</a>
     <a href="./orders">Orders</a>
     <a href="./reports">Reports</a>
